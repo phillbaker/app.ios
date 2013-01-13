fb1ca2f608b6762fd88ffcab0e1753c87d47acac
Phill Baker <phill@generalassemb.ly>
2013-01-13 14:34:57 -0500

    Initial commit on master

---
+++ AppTests/AppTests.h
@@ -0,0 +1,13 @@
+//
+//  AppTests.h
+//  AppTests
+//
+//  Created by phill on 1/13/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AppTests : SenTestCase
+
+@end
---
+++ AppTests/AppTests.m
@@ -0,0 +1,32 @@
+//
+//  AppTests.m
+//  AppTests
+//
+//  Created by phill on 1/13/13.
+//
+//
+
+#import "AppTests.h"
+
+@implementation AppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AppTests");
+}
+
+@end
